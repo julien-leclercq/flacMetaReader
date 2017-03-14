4c834bc651b29507b623c48dacc80fb0aca5b55e
julien-leclercq <julien@MacBook-Pro-de-Julien.local>
2017-03-14 21:51:18 +0100

    first commit

---
+++ lib/flacMetaReader.ex
@@ -0,0 +1,55 @@
+defmodule FlacMetaReader do
+  
+  @moduledoc """
+  Small library to parse and extract Flac files metadata
+  """
+  @flac_header <<0x66, 0x4C, 0x61, 0x43>>
+  @vorbis_comment 4
+  
+  def read(file) do
+    File.open(file)
+    |> init_flac_meta_reading
+  end
+  
+  @doc """
+    await a couple atom, PID the PID being the file descriptor of
+    the subject track
+  """
+  def init_flac_meta_reading({:error, reason}),  do: {:error, reason}
+  def init_flac_meta_reading({:ok, io_device}) do
+    case IO.binread(io_device, :all) do
+      @flac_header <> tail -> {:ok, tail}
+      _ -> {:error, 'file is not valid FLAC'}
+    end
+  end
+  
+  @doc """
+  
+  """
+  def read_metadatas(bits), do: read_metadatas(bits, %{})
+  def read_metadatas(bits, metadatas) do
+    <<
+      final::size(1),
+      block_type::size(7),
+      length::size(24),
+      meta_data_block::size(length)-unit(8),
+      ending::binary
+    >> = bits
+    
+    res = case block_type do
+      @vorbis_comment ->
+        Map.put(
+          metadatas,
+          :vorbis,
+          Vorbis.read_block(<<meta_data_block::size(length)-unit(8)>>)
+        )
+      _ -> metadatas
+    end
+    
+    case final do
+      1 -> res
+      _ -> read_metadatas(<<ending::binary>>, res)
+    end
+  end
+  
+end
---
+++ test/flacMetaReader_test.exs
@@ -0,0 +1,8 @@
+defmodule FlacMetaReaderTest do
+  use ExUnit.Case
+  doctest FlacMetaReader
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
