ab0b2444ea99fc3b5a46679a177308becf230bb6
julien-leclercq <julien.leclercq@derniercri.io>
2017-08-01 11:35:48 +0200

    add credo

--- mix.exs
+++ mix.exs
@@ -28,6 +28,8 @@ defmodule FlacMetaReader.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    ]
   end
 end
