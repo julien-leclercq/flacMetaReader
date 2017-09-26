b96589c0d10365f394fb1502676e8cc06f7e4b4b
julien-leclercq <julien.leclercq@derniercri.io>
2017-09-26 23:46:36 +0200

    change app name to koop

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule FlacMetaReader.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :flacMetaReader,
+    [app: :koop,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
